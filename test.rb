9a9577aee6766f3b3c17d3fcd07c33f499a6d207
Daniel Sandbecker <daniel.sandbecker@hb.se>
2022-09-27 08:45:25 +0200

    Add option to read and parse input line by line
    Passing "-1" as the first argument will now read one input line at a
    time, parsing it and outputting the *unwrapped* message (the parser
    produces an array of messages) as a JSON object. With several lines, the
    complete output will not be valid JSON, but tools like `jq` can accept a
    series of objects as input.

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require_relative 'environment'
 
 require 'json'
@@ -7,11 +9,17 @@ require 'sip2/transformer'
 parser = Sip2::Parser.new
 transformer = Sip2::Transformer.new
 
-data = ARGF.read
-intermediate_tree = parser.parse_with_debug(data)
-tree = transformer.apply(intermediate_tree)
-puts tree.to_json
+parse = ->(data) {
+  intermediate_tree = parser.parse_with_debug(data)
+  transformer.apply(intermediate_tree)
+}
 
+if ARGV.first == "-1"
+  ARGV.shift
 
-
-
+  ARGF.each_line do |data|
+    puts parse.call(data).first.to_json
+  end
+else
+  puts parse.call(ARGF.read).to_json
+end
