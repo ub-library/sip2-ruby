81dd310f403178bdde37f7d94e312d1bdaefdc77
Daniel Sandbecker <daniel.sandbecker@hb.se>
2025-06-05 13:44:43 +0200

    Update nixpkgs to 25.05 and ruby in dev env to 3.4
    The updated ruby introduced a problem where bundler would warn about
    missing extensions for default gems. I introduce a patch to silence the
    warning. The patch was found in this related nixpkgs issue:
    
    https://github.com/NixOS/nixpkgs/issues/400243

--- flake.nix
+++ flake.nix
@@ -1,17 +1,32 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        rubyOverlay = final: prev: {
+          ruby_3_4 = prev.ruby_3_4.override {
+            bundler = prev.bundler.overrideAttrs ({ postFixup ? "", ... }: {
+              postFixup = postFixup + ''
+                stubSpec=$(find $out -path '*bundler/stub_specification.rb')
+                substituteInPlace $stubSpec \
+                  --replace-fail 'warn "Source #{source} is ignoring #{self} because it is missing extensions"' '# redacted because of https://github.com/NixOS/nixpkgs/issues/400243'
+              '';
+            });
+          };
+        };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rubyOverlay ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.ruby
+            pkgs.ruby_3_4
           ];
         };
       });
